64b0aaa7d61d98b0820640b041f5b5aa4e25b494
nozomu-fukumoto <142648679+nozomu-fukumoto@users.noreply.github.com>
2023-12-20 10:36:20 +0900

    feat：SPデザイン修正

---
+++ src/test/java/com/example/portfolio/fn/FnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolio.fn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
